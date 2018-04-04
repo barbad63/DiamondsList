58a0498cf8478f1128349e5bc318e2847158b1cc
anithasunil2008 <anithasunil2008@gmail.com>
2018-04-03 20:36:50 -0400

    added star rating and category.handlebars file

--- public/js/app.js
+++ public/js/app.js
@@ -52,3 +52,74 @@ $("#serviceFormSubmit").on("click", function(event) {
     $("#amount").val("");
     $("#description").val("");
 });
+
+class StarRating extends HTMLElement {
+    get Value() {
+        return this.getAttribute('value') || 0;
+    }
+
+    set value(val) {
+        return this.setAttribute('value', val);
+        this.highlight(this.value - 1);
+    }
+
+    get number() {
+        return this.getAttribute('number') || 5;
+    }
+
+    set number(val) {
+        this.setAttribute('number', val);
+
+
+        this.stars = [];
+
+        while (this.firstChild) {
+            this.removeChild(this.firstChild);
+        }
+        this.value = this.value;
+
+        for (var i = 0; i < this.number; i++) {
+            var s = document.createElement('div');
+            s.className = 'star';
+            this.appendChild(s);
+            this.stars.push(s);
+        }
+    }
+    highlight(index) {
+        this.stars.forEach((star, i) => {
+            star.classList.toggle('full', i <= index);
+        });
+    }
+
+    constructor() {
+        super();
+
+        this.number = this.number;
+
+        this.addEventListener('mousemove', e => {
+            var box = this.getBoundingClientRect(),
+                starIndex = Math.floor((e.pageX - box.left) / box.width * this.stars.length);
+            console.log(starIndex);
+            this.highlight(starIndex);
+        });
+
+        this.addEventListener('mouseout', () => {
+            this.value = this.value;
+        });
+
+        this.addEventListener('click', e => {
+            var box = this.getBoundingClientRect(),
+                starIndex = Math.floor((e.pageX - box.left) / box.width * this.stars.length);
+            this.value = starIndex + 1;
+
+            var rateEvent = new Event('rate');
+            this.dispatchEvent(rateEvent);
+        });
+    }
+}
+
+rating.addEventListener('rate', () => {
+    console.log("Rating: " + rating.value);
+});
+
+window.customElements.define('x-star-rating', StarRating);
