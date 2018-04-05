1078e85a3a82357cf735deab3c6e8d60c5c13fcb
anithasunil2008 <anithasunil2008@gmail.com>
2018-04-05 17:21:00 -0400

    untracking .vscode folder

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ var BetterMemoryStore = require(__dirname + '/memory');
 
 var store = new BetterMemoryStore({ expires: 60 * 60 * 1000, debug: true })
 
+var db = require("./models");
+
 
 // Create an instance of the express app.
 var app = express();
@@ -61,21 +63,23 @@ passport.use('local', new LocalStrategy({
     console.log(username + ' = ' + password);
     if (!username || !password) { return done(null, false, req.flash('message', 'All fields are required.')); }
     var salt = '7fa73b47df808d36c5fe328546ddef8b9011b2c6';
-    connection.query("select * from tbl_users where username = ?", [username], function(err, rows) {
-        console.log(err);
-        if (err) return done(req.flash('message', err));
+    // connection.query("select * from tbl_users where username = ?", [username],
+    db.Creds.findAll({ where: { username: username } }).then(
+        function(err, rows) {
+            console.log(err);
+            if (err) return done(req.flash('message', err));
 
-        if (!rows.length) { return done(null, false, req.flash('message', 'Invalid username or password.')); }
-        salt = salt + '' + password;
-        var encPassword = crypto.createHash('sha1').update(salt).digest('hex');
-        var dbPassword = rows[0].password;
+            if (!rows.length) { return done(null, false, req.flash('message', 'Invalid username or password.')); }
+            salt = salt + '' + password;
+            var encPassword = crypto.createHash('sha1').update(salt).digest('hex');
+            var dbPassword = rows[0].password;
 
-        if (!(dbPassword == encPassword)) {
-            return done(null, false, req.flash('message', 'Invalid username or password.'));
-        }
-        req.session.user = rows[0];
-        return done(null, rows[0]);
-    });
+            if (!(dbPassword == encPassword)) {
+                return done(null, false, req.flash('message', 'Invalid username or password.'));
+            }
+            req.session.user = rows[0];
+            return done(null, rows[0]);
+        });
 }));
 
 passport.serializeUser(function(user, done) {
@@ -86,6 +90,25 @@ passport.deserializeUser(function(id, done) {
     connection.query("select * from tbl_users where id = " + id, function(err, rows) {
         done(err, rows[0]);
     });
+});
+
+// Sign-In routes
+// app.get('/signin', function(req, res) {
+//     res.render('login/index', { 'message': req.flash('message') });
+// });
+
+app.post("/signin", passport.authenticate('local', {
+    successRedirect: '/category',
+    failureRedirect: '/signIn',
+    failureFlash: true
+}), function(req, res, info) {
+    res.render('/category', { 'message': req.flash('message') });
+});
+
+app.get('/logout', function(req, res) {
+    req.session.destroy();
+    req.logout();
+    res.redirect('/signIn');
 });
 
 
