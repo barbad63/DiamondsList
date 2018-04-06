2f16e746cd72d9cbd83d433d03517bde3e89168a
anithasunil2008 <anithasunil2008@gmail.com>
2018-04-06 14:11:23 -0400

    passport local auth working now

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(sess({
     saveUninitialized: true
 }));
 
-app.use(logger('combined'));
+app.use(logger('dev'));
 app.use(cookieParser());
 
 var PORT = process.env.PORT || 8080;
@@ -54,6 +54,12 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Error handling middleware
+app.use(function(err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+});
+
 
 // passport Strategy -- the express session middleware before calling passport.session()
 passport.use('local', new LocalStrategy({
@@ -65,9 +71,11 @@ passport.use('local', new LocalStrategy({
     if (!username || !password) { return done(null, false, req.flash('message', 'All fields are required.')); }
     var salt = '7fa73b47df808d36c5fe328546ddef8b9011b2c6';
     // connection.query("select * from tbl_users where username = ?", [username],
-    db.Creds.findAll({ where: { username: username } }).then(
-        function(err, rows) {
-            console.log(err);
+    db.Creds.findAll({ where: { "username": username } }).then(
+        function(rows, err) {
+            console.log("Error Occured while retrieving user creds = " + JSON.stringify(err));
+            console.log("Returned rows = " + rows);
+
             if (err) return done(req.flash('message', err));
 
             if (!rows.length) { return done(null, false, req.flash('message', 'Invalid username or password.')); }
@@ -84,35 +92,34 @@ passport.use('local', new LocalStrategy({
 }));
 
 passport.serializeUser(function(user, done) {
+    console.log("Inside serializeUser");
     done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
     // connection.query("select * from tbl_users where id = " + id, 
     db.Creds.findAll({ where: { id: id } }).then(
-        function(err, rows) {
+        function(rows, err) {
+            console.log("Inside deserializeUser");
+            console.log("rows = " + rows);
             done(err, rows[0]);
         });
 });
 
-// Sign-In routes
-// app.get('/signin', function(req, res) {
-//     res.render('login/index', { 'message': req.flash('message') });
-// });
 
 app.post("/signIn", passport.authenticate('local', {
-    successRedirect: '/category',
+    successRedirect: '/services',
     failureRedirect: '/signIn',
     failureFlash: true
 }), function(req, res, info) {
-    res.render('/category', { 'message': req.flash('message') });
+    res.render('/services', { 'message': req.flash('message') });
 });
 
-// app.get('/logout', function(req, res) {
-//     req.session.destroy();
-//     req.logout();
-//     res.redirect('/signIn');
-// });
+app.get('/logout', function(req, res) {
+    req.session.destroy();
+    req.logout();
+    res.redirect('/signIn');
+});
 
 
 require("./routes/service-api-routes.js")(app);
